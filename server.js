466555ba9798f238340386c4a6b6fe90df0ea1db
mengjingnan <mengjingnan@gmail.com>
2016-12-05 15:31:25 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+/**
+ * Created by mengjingnan on 2016/12/5.
+ */
+
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
